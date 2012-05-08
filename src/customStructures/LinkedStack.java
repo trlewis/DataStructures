d6b695c6325654510334831411831d231a66ad71
Travis Lewis <econobeing5@gmail.com>
2012-05-08 14:57:08 -0700

    initial commit 2...

--- src/customStructures/LinkedStack.java
+++ src/customStructures/LinkedStack.java
@@ -1,6 +1,60 @@
 package customStructures;
 
-public class LinkedStack implements IStack<T>
+public class LinkedStack<T> implements IStack<T>
 {
+    private class Node<T>
+    {
+        T element;
+        Node<T> previous;
+    }
+    
+    private Node<T> head;
+    
+    public LinkedStack()
+    {
+        head = null;
+    }
+    
+    public LinkedStack(T element)
+    {
+        head = new Node<T>();
+        head.element = element;
+    }
+    
+    @Override
+    public void push(T element)
+    {
+        Node<T> n = new Node<T>();
+        n.element = element;
+        
+    }
+
+    @Override
+    public T pop()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T peek()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
 }
